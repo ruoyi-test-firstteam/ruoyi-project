61b72ae8116da8e9f0a630ba655668cdc9293f01
ruoyi-test-firstteam <1102343495@qq.com>
2020-09-30 18:59:02 +0800

    删除模块

--- ruoyi-company/src/main/java/com/ruoyi/company/service/IJmrBaseService.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.company.service;
-
-import com.ruoyi.company.domain.JmrBase;
-
-public interface IJmrBaseService {
-    //存储匹配值信息
-    public void save(JmrBase studentMatchResult);
-
-}
