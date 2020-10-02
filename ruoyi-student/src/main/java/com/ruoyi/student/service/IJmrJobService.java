4b50f908e85c6853079dacbcab603dd7b7d44820
zhangwen <1484131891@qq.com>
2020-10-02 12:28:41 +0800

    bhjbhjhj

--- ruoyi-student/src/main/java/com/ruoyi/student/service/IJmrJobService.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.student.service;
-
-import com.ruoyi.student.domain.JmrJob;
-
-import java.util.List;
-
-public interface IJmrJobService {
-    public List<JmrJob> selectList();
-}
